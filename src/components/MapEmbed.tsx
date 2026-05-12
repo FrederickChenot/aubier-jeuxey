3cad2a795c68449a3bcbe5bd6a373ea989a07e48
Fred'Dev <fredchenot@gmail.com>
2026-05-12 22:25:35 +0200

    fix: iframe Google Maps avec vrai place ID (44a Rue d'Épinal, Jeuxey)

--- src/components/MapEmbed.tsx
+++ src/components/MapEmbed.tsx
@@ -2,7 +2,7 @@ export default function MapEmbed() {
   return (
     <div className="w-full rounded-xl overflow-hidden shadow-md">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2676!2d6.4097!3d48.1833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479276bfb1234567%3A0xabcdef123456!2s44A+Rue+d%27%C3%89pinal%2C+88000+Jeuxey%2C+France!5e0!3m2!1sfr!2sfr!4v1715000000000"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3154.8369476573494!2d6.485419276726074!3d48.19465244718566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4793a04aed5ddbb5%3A0x459dec8b31c1f8c6!2s44a%20Rue%20d&#39;Epinal%2C%2088000%20Jeuxey!5e1!3m2!1sfr!2sfr!4v1778617475032!5m2!1sfr!2sfr"
         width="100%"
         className="h-[280px] md:h-[420px] w-full border-0"
         allowFullScreen
